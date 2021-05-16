7403b7ec9c97039e68b2ca6e0588d3456388d027
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-05-15 22:35:03 -0400

    added image of app and read me

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
+
 export default function Home({results}) {
+
+  
   return (
     <div >
       <Head>
@@ -23,6 +26,7 @@ export default function Home({results}) {
 }
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+
   const request = await fetch(
     `https://api.themoviedb.org/3${
       requests[genre]?.url || requests.fetchTrending.url
