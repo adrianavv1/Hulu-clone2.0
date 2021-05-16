172d1bbfd3a2815f3ec322147abfd0bb3ea33f61
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-05-15 20:28:52 -0400

    added thumbnail.js for pictures of shows/movies

--- hulu-clone2.0/components/Thumbnail.js
+++ hulu-clone2.0/components/Thumbnail.js
@@ -1,8 +1,9 @@
 import Image from 'next/image';
+import { ThumbUpIcon } from "@heroicons/react/outline";
 function Thumbnail({ result }) {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div>
+        <div className='p-2 group cursor-pointer'>
             <Image 
                 layout='responsive'
                 src={
@@ -12,8 +13,23 @@ function Thumbnail({ result }) {
                 height={1080}
                 width={1920}
             />
-            <div className="p-2">
+            <div className='p-2'>
                 <p className='truncate max-w-md'>{result.overview}</p>
+
+                <h2 className="mt-1 text-2xl text-white transition-all
+                 duration-100 ease-in-out group-hover:font-bold">
+                    {result.title || result.original_name}
+                    </h2>
+
+                
+                <p className='flex items-center opacity-0 
+                group-hover:opacity-100'>
+                    {result.media_type && `${result.media_type} •`}{" "}
+                    {result.release_date || result.first_air_date} •{" "}
+                    <div>
+                    <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
+                    </div>
+                </p>
             </div>
         </div>
     );
